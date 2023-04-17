3271c437c2932a46071fa7f6dec8de8b2312f1f2
VitalikGrischenko <110487381+VitalikGrischenko@users.noreply.github.com>
2023-04-17 19:41:53 +0300

    rre

---
+++ js/2.js
@@ -0,0 +1,12 @@
+// Порахуйте суму чисел від 12 до 27
+
+// for(let i = 1; i <= 10; i += 1){
+// console.log(i)
+// }
+
+
+let numder = 0;
+for (let i = 12; i <= 27; i += 1) {
+  console.log(numder += i);
+}
+
