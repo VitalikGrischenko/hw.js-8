d5aca5e3a29249afd2e380aed4d49658b9c8fcc7
VitalikGrischenko <110487381+VitalikGrischenko@users.noreply.github.com>
2023-04-20 17:22:23 +0300

    24315

--- js/8.js
+++ js/8.js
@@ -3,4 +3,19 @@
 
 
 
+let grades = [];
+let average = 0;
 
+for (let i = 1; ; i++) {
+  let grade = prompt("Введіть оцінку " + i + ":");
+  if (grade === null || grade === "") {
+    break;
+  } else {
+    grades.push(parseFloat(grade));
+    average = grades.reduce((a, b) => a + b) / grades.length; 
+    console.log("Середній бал успішності: " + average.toFixed(2)); 
+    if (average < 7) { 
+      break;
+    }
+  }
+}
